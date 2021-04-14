9341f41b524498b2f5a3bc66b763a88d25486483
SHANTOSH KSHETRI <75415500+shantosh123@users.noreply.github.com>
2021-04-14 20:23:03 +0300

    read to update file
    this job is done

---
+++ ReadToUpdate.java
@@ -0,0 +1,49 @@
+
+package app;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+import data.Participants;
+
+
+/**
+ * Servlet implementation class ReadToUpdate
+ */
+@WebServlet("/readtoupdate")
+public class ReadToUpdate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Dao dao;
+	public void init() {
+		dao=new Dao("jdbc:mysql://localhost:3308/ourelectionproject", "root", "root");
+	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReadToUpdate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id=request.getParameter("id");
+		Participants Parti=null;
+		if (dao.Connect()) {
+			Parti=dao.readParticipants(id);
+		}
+		request.setAttribute("participants", Parti);
+		
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/ShowParticipantsToEdit.jsp");
+		rd.forward(request, response);
+	}
+}
